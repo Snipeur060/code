dbfb51f77eba798315d06c3f1cb614d3cd665ebe
sitesgithub <36903811+sitesgithub@users.noreply.github.com>
2019-05-26 18:24:13 -0600

    Update main0.js

--- main0.js
+++ main0.js
@@ -23,3 +23,52 @@ getGiftCode = function () {
     document.getElementById('b').value += code;
 
 } //generates codes
+
+net = function () {
+  document.getElementById('d').play();
+  document.getElementById('long').readOnly = 'true';
+  document.getElementById('long').value = '11';
+  getGiftCode = function () {
+    let code = '';
+    let dict = '0123456789';
+    for(var i = 0; i < 11; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    code += '\n'
+    document.getElementById('b').value += code;
+
+}
+}
+
+apple = function () {
+  document.getElementById('d').play();
+  document.getElementById('long').readOnly = 'true';
+  document.getElementById('long').value = '16';
+    getGiftCode = function () {
+    let code = '';
+    let dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for(var i = 0; i < 15; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    code += '\n'
+    code = 'X'+code
+    document.getElementById('b').value += code;
+
+}
+}
+
+reset = function () {
+  document.getElementById('long').readOnly = false;
+  document.getElementById('long').value = '16';
+    getGiftCode = function () {
+    let code = '';
+    let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for(var i = 0; i < 15; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    code += '\n'
+    code = 'X'+code
+    document.getElementById('b').value += code;
+
+}
+}
