96af550ec89dc9396ae64f45e9163e7ef4c14ff1
sitesgithub <36903811+sitesgithub@users.noreply.github.com>
2019-06-10 19:15:26 -0600

    Update and rename main0.js to main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ var textarea = document.getElementById('a')
 if (navigator.userAgent.indexOf('Mobile') !== -1) { document.getElementById('save').style.display = "none"; textarea.style.height = "100px";
 }//resize for mobile
 getGiftCode = function () {
+document.getElementById('netflixprefix').style.display = 'none';
     let code = '';
     let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     for(var i = 0; i < document.getElementById('long').value; i++){
@@ -27,23 +28,45 @@ getGiftCode = function () {
 net = function () {
   document.getElementById('d').play();
   document.getElementById('long').readOnly = 'true';
-  document.getElementById('long').value = '11';
+  document.getElementById('long').value = '12';
+  document.getElementById('netflixprefix').style.display = 'block';
   getGiftCode = function () {
     let code = '';
+    let ler = '';
+    let netflix = '';
     let dict = '0123456789';
-    for(var i = 0; i < 11; i++){
+    let random3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (var b = 0; b < 3; b++) {
+        ler = ler + random3.charAt(Math.floor(Math.random() * random3.length));
+    }
+    for(var i = 0; i < 6; i++){
         code = code + dict.charAt(Math.floor(Math.random() * dict.length));
     }
-    code += '\n'
-    document.getElementById('b').value += code;
+        ler = ler + code;
+        code = ler;
+    if (document.getElementById('netflix').value === '') {
+        for (var c = 0; c < 3; c++) {
+        netflix = netflix + random3.charAt(Math.floor(Math.random() * random3.length));
+        }
+        code = netflix + code
+    } else {
+      code = document.getElementById('netflix').value + code;
+      code += '\n';
+      document.getElementById('b').value += code;
+    }
+        code = document.getElementById('netflix').value + code;
+        code += '\n';
+        document.getElementById('b').value += code;
+    }
+        }
 
-}
-}
+
 
 apple = function () {
   document.getElementById('d').play();
   document.getElementById('long').readOnly = 'true';
   document.getElementById('long').value = '16';
+  document.getElementById('netflixprefix').style.display = 'none';
     getGiftCode = function () {
     let code = '';
     let dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -60,14 +83,14 @@ apple = function () {
 reset = function () {
   document.getElementById('long').readOnly = false;
   document.getElementById('long').value = '16';
+  document.getElementById('netflixprefix').style.display = "none"
     getGiftCode = function () {
     let code = '';
     let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for(var i = 0; i < 15; i++){
+    for(var i = 0; i < 16; i++){
         code = code + dict.charAt(Math.floor(Math.random() * dict.length));
     }
     code += '\n'
-    code = 'X'+code
     document.getElementById('b').value += code;
 
 }
